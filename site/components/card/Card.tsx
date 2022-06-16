4dd26822139b39ae7c1dc015bbdc5fb7505b9d86
Dimitar Chetelev <chetelev@gmail.com>
2022-06-16 23:20:29 +0200

    card styles redesign

--- site/components/card/Card.tsx
+++ site/components/card/Card.tsx
@@ -1,35 +1,37 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 interface CardProps {
-    title?: string
-    description: string
-    image: string
-    tags: Array<string>
-  }
-
- const Card: React.FC<CardProps> = ({
-    title,
-    description,
-    image,
-    tags
-  }) => {
-    return (
-        <div className="rounded overflow-hidden shadow-lg">
-            <Image className="w-1/2" width={500} height={500} src={image} alt="" />
-            <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{title}</div>
-            <p className="text-gray-700 text-base">
-                {description}
-            </p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-            { 
-                tags.map(
-                    tag => <span key={tag} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
-                )
-            }
-            </div>
-        </div>
-    )
+  title?: string;
+  description: string;
+  image: string;
+  tags: Array<string>;
 }
-export default Card
+
+const Card: React.FC<CardProps> = ({ title, description, image, tags }) => {
+  return (
+    <>
+      <div className="flex flex-col h-100 rounded shadow-xl">
+        <div className="">
+          <div className="font-bold text-xl mb-2">{title}</div>
+          <p className="text-gray-700u text-base">{description}</p>
+        </div>
+        <div className="">
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className=" bg-sky-700 w-full h-3"></div>
+      </div>
+
+      <div className="rounded overflow-hidden shadow-xl">
+        <Image width={700} height={700} src={image} alt="" />
+      </div>
+    </>
+  );
+};
+export default Card;
--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['media-exp2.licdn.com'],
+  },
 }
 
 module.exports = nextConfig
