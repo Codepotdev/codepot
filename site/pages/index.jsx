e2f9c094e8e0c4df5f9434129a59a598a641a81a
Dimitar Chetelev <chetelev@gmail.com>
2022-07-28 16:41:08 +0200

    Add Github endpoint and aggregate with stackoverflow

--- site/pages/index.jsx
+++ site/pages/index.jsx
@@ -1,34 +1,37 @@
 import { Card } from "@components/card";
-import { getLinkPreview } from "link-preview-js";
+import { shuffleArray } from "@lib/shuffle";
 
 export const getStaticProps = async () => {
   const stackOverflowAPI = await fetch(
-    "https://api.stackexchange.com/2.3/questions/no-answers?page=1&pagesize=64&order=desc&max=1654128000&sort=activity&site=stackoverflow"
+    "https://api.stackexchange.com/2.3/questions/no-answers?page=1&pagesize=10&order=desc&max=1654128000&sort=activity&site=stackoverflow"
   );
 
+  const gitHubAPI = await fetch("https://api.github.com/users/mindsdb/repos");
+
   const stackOverflowAPIResponse = await stackOverflowAPI.json();
+  const gitHubAPIResponse = await gitHubAPI.json();
 
-  for (const item of stackOverflowAPIResponse.items) {
-    item.metadata = await getLinkPreview(item.link);
-  }
-
+  const response = [...stackOverflowAPIResponse.items, ...gitHubAPIResponse];
+  shuffleArray(response);
   return {
-    props: { questions: stackOverflowAPIResponse.items },
+    props: { cardData: response },
   };
 };
 
-const Home = ({ questions }) => {
+const Home = ({ cardData }) => {
+  console.log(cardData);
   return (
     <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
-      {questions.map((question) => (
+      {cardData.map((cd) => (
         <Card
-          key={question.question_id}
-          title={question.title}
-          description={question.owner.display_name}
-          profileImage={question.owner.profile_image}
-          tags={question.tags}
-          link={question.link}
-          metadata={question.metadata}
+          title={cd.title ? cd.title : cd.full_name}
+          description={cd.description}
+          image={
+            cd.owner.avatar_url ? cd.owner.avatar_url : "/stackoverflow.svg"
+          }
+          tags={cd.tags ? cd.tags : cd.topics}
+          link={cd.link ? cd.link : cd.url}
+          key={cd.question_id ? cd.question_id : cd.id}
         />
       ))}
     </div>
