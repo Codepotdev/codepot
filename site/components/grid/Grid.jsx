5178e1c83c2cea6921ee4e3607c8a5eccd87d228
Dimitar Chetelev <chetelev@gmail.com>
2022-08-31 16:22:39 +0200

    modal fixes

--- site/components/grid/Grid.jsx
+++ site/components/grid/Grid.jsx
@@ -1,6 +1,7 @@
 import Card from "@components/card/Card";
 
 export default function Grid({ cardData }) {
+  console.log(cardData)
   return (
     <>
       <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
@@ -11,7 +12,8 @@ export default function Grid({ cardData }) {
             image={cd.url ? cd.url : "/stackoverflow.svg"}
             tags={cd.tags ? cd.tags : cd.topics}
             link={cd.link ? cd.link : cd.url}
-            key={cd.question_id ? cd.question_id : cd.id}
+            id={cd.id ? cd.id : cd.question_id}
+            key={cd.id ? cd.id : cd.question_id}
           />
         ))}
       </div>
