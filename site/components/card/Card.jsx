0bd29b5863c117b5c380d2134689a6f9a521a993
chetelev <chetelev@gmail.com>
2022-09-26 23:13:35 +0200

    card message

--- site/components/card/Card.jsx
+++ site/components/card/Card.jsx
@@ -8,10 +8,11 @@ export default function Card({
   tags,
   link,
   id,
-  type,
-  language,
   imagesrc,
+  color,
+  message,
 }) {
+  console.log(color);
   return (
     <>
       <Link
@@ -30,7 +31,7 @@ export default function Card({
         }}
         as={`/contributions/${id}`}
       >
-        <div className="flex flex-col border-2 border-slate-100 hover:border-slate-200 bg-slate-200/50 rounded-3xl overflow-hidden p-2 shadow-lg cursor-pointer">
+        <div className="flex flex-col border-2 hover:border-slate-300 bg-slate-200/50 rounded-3xl overflow-hidden p-2 shadow-lg cursor-pointer">
           <div className="flex flex-row px-6 py-2 justify-between items-center">
             {image && (
               <Image
@@ -55,14 +56,18 @@ export default function Card({
           </div>
           <div className="mb-auto"></div>
           <div className="px-6 pt-4 pb-2">
-            {tags?.slice(0, 2).map((tag) => (
-              <span
-                key={tag}
-                className="inlipne-block bg-slate-50 border-2 rounded-full px-3 py-1 text-sm mr-1 mb-1"
-              >
-                {"#" + tag}
-              </span>
-            ))}
+            {/* {tags.length > 1 &&
+              tags?.slice(0, 1).map((tag) => (
+                <span
+                  key={tag}
+                  className="flex flex-row bg-slate-50 justify-center capitalize rounded-sm px-3 py-1 text-sm mr-1 mb-1"
+                >
+                  {(tag)}
+                </span>
+              ))} */}
+            <span className="flex flex-row bg-slate-50 justify-center capitalize rounded-sm px-3 py-1 text-sm mr-1 mb-1">
+              {message}
+            </span>
           </div>
         </div>
       </Link>
